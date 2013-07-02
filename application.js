03725a73c846d119287282c3d7d15c65443a5564
supertopher <asimplechris@gmail.com>
2013-07-02 16:07:32 -0700

    Possibly completed the assignment, not sure what protypical inheritence is

--- application.js
+++ application.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
 
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
+
+  var addDice = function() {
+    $('.dice').append('<div class="die">0</div>');
+  };
+
+  var getRandomNumber = function(k, die) {
       var value = Math.floor((Math.random()*6)+1);
       $(die).text(value);
-    });
-  });
+    }
+
+  var rollDice = function() {
+    $('.die').each(getRandomNumber);
+  };
+
+  $('#roller button.add').on('click', addDice);
+
+  $('#roller button.roll').on('click', rollDice);
 });
